f280fbbc5dfb63fc52baed6e0323595cd532e80f
dhirendra441 <dhirendrapratapdindoure@gmail.com>
2025-05-22 22:46:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeMessage() {
+    document.getElementById('message').textContent = "Download complete!";
+    document.getElementById('message').style.color = "green";
+    document.getElementById('message').style.fontSize = "20px";
+    document.getElementById('message').style.fontWeight = "bold";
+    document.getElementById('message').style.textAlign = "center";
+    document.getElementById('message').style.marginTop = "20px";
+    document.getElementById('message').style.transition = "all 0.5s ";
+    
+ 
+  }
+  
